1d90e4c75bc29c8824fc6f97bb3b95c390d91169
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-22 18:50:33 -0300

    feat(vitest): add  vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'node:path';
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		globals: true,
+		root: './',
+		environment: 'node',
+		include: ['**/*.test.ts'],
+		coverage: {
+			reportsDirectory: './coverage',
+			reporter: ['text', 'html', 'lcov', 'cobertura'],
+			provider: 'v8',
+			include: ['src/**/*.ts'],
+			exclude: [
+				'**/types/**',
+				'**/*.d.ts',
+				'**/mocks/**',
+				'**/*.dto.ts',
+				'**/*.entity.ts',
+			],
+		},
+	},
+	plugins: [
+		tsConfigPaths(),
+		swc.vite({
+			module: { type: 'es6' },
+		}),
+	],
+	resolve: {
+		alias: {
+			src: resolve(__dirname, './src'),
+		},
+	},
+});

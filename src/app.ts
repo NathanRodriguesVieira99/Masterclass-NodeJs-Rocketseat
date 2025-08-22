96356f186a81eed8a13751e0bfed59cd5bad1127
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-22 18:47:34 -0300

    refactor(server): replace app.ts and server.ts files to src folder

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+import { fastifySwagger } from '@fastify/swagger';
+import scalarAPIReference from '@scalar/fastify-api-reference';
+import fastify from 'fastify';
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { createCoursesRoute } from '@/routes/create-course';
+import { getCoursesByIdRoute } from '@/routes/get-course-by-id';
+import { getCoursesRoute } from '@/routes/get-courses';
+
+/**
+ * FASTIFY
+ */
+export const server = fastify({
+	logger: {
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				translateTime: 'HH:MM:ss Z',
+				ignore: 'pid,hostname',
+			},
+		},
+	},
+}).withTypeProvider<ZodTypeProvider>();
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
+
+/**
+ *  SWAGGER
+ */
+if (process.env.NODE_ENV === 'development') {
+	server.register(fastifySwagger, {
+		openapi: {
+			info: {
+				title: 'Desafio NodeJs',
+				version: '1.0.0',
+			},
+		},
+		transform: jsonSchemaTransform,
+	});
+
+	server.register(scalarAPIReference, {
+		routePrefix: '/docs',
+		configuration: {
+			theme: 'kepler',
+		},
+	});
+}
+
+/**
+ * Rotas
+ */
+server.register(createCoursesRoute);
+server.register(getCoursesByIdRoute);
+server.register(getCoursesRoute);

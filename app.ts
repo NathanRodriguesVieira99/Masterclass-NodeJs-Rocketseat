8f343772ae86f16815f0547a9bdac181f9ac40c8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-21 23:32:16 -0300

    feat(app): initialize Fastify server with Swagger and routes

---
+++ app.ts
@@ -0,0 +1,57 @@
+import fastify from "fastify";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from '@fastify/swagger';
+import { createCoursesRoute } from "./src/routes/create-course";
+import { getCoursesByIdRoute } from "./src/routes/get-course-by-id";
+import { getCoursesRoute } from "./src/routes/get-courses";
+import scalarAPIReference from '@scalar/fastify-api-reference'
+
+/**
+ * FASTIFY
+ */
+export const server = fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+}).withTypeProvider<ZodTypeProvider>();
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
+
+/**
+ *  SWAGGER
+ */
+server.register(fastifySwagger,{
+  openapi: {
+    info:{
+      title: 'Desafio NodeJs',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+server.register(scalarAPIReference,{
+routePrefix: '/docs',
+configuration:{
+  theme: 'kepler'
+}
+})
+
+/**
+ * Rotas
+ */
+server.register(createCoursesRoute)
+server.register(getCoursesByIdRoute)
+server.register(getCoursesRoute)

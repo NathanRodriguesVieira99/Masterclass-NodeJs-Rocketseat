411d2271dc4c6a5515658372b8c5fc25b25cd8d4
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-24 12:23:25 -0300

    test(crate course): add authorization for create course test

--- src/routes/create-course.test.ts
+++ src/routes/create-course.test.ts
@@ -1,13 +1,17 @@
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import request from 'supertest';
 import { server } from '@/app';
+import { MakeAuthenticatedUser } from '@/test/factories/make-user';
 
 beforeAll(async () => await server.ready());
 
 it('create a course', async () => {
+	const { token } = await MakeAuthenticatedUser('manager');
+
 	const response = await request(server.server)
 		.post('/courses')
 		.set('Content-Type', 'application/json')
+		.set('Authorization', token)
 		.send({ title: faker.lorem.words(4) });
 
 	expect(response.status).toBe(201);

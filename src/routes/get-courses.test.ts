d34cd00725164cf071954527da02437b737254c7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-24 12:20:10 -0300

    test(get courses): add authorization to get all courses test

--- src/routes/get-courses.test.ts
+++ src/routes/get-courses.test.ts
@@ -3,17 +3,19 @@ import request from 'supertest';
 import { server } from '@/app';
 
 import { MakeCourse } from '@/test/factories/make-course';
+import { MakeAuthenticatedUser } from '@/test/factories/make-user';
 
 beforeAll(async () => await server.ready());
 
 it('get all courses', async () => {
 	const titleId = randomUUID();
 
+	const { token } = await MakeAuthenticatedUser('manager');
 	await MakeCourse(titleId);
 
-	const response = await request(server.server).get(
-		`/courses?search=${titleId}`,
-	);
+	const response = await request(server.server)
+		.get(`/courses?search=${titleId}`)
+		.set('Authorization', token);
 
 	expect(response.status).toBe(200);
 	expect(response.body).toEqual({

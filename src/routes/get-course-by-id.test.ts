ce9a59055dd091ee56b134671835d594ea3f65ab
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-24 12:22:15 -0300

    test(get course by id): add authorization to tests

--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -2,13 +2,17 @@ import { randomUUID } from 'node:crypto';
 import request from 'supertest';
 import { server } from '@/app';
 import { MakeCourse } from '@/test/factories/make-course';
+import { MakeAuthenticatedUser } from '@/test/factories/make-user';
 
 beforeAll(async () => await server.ready());
 
 it('get a course by id', async () => {
+	const { token } = await MakeAuthenticatedUser('student');
 	const course = await MakeCourse();
 
-	const response = await request(server.server).get(`/courses/${course.id}`);
+	const response = await request(server.server)
+		.get(`/courses/${course.id}`)
+		.set('Authorization', token);
 
 	expect(response.status).toBe(200);
 	expect(response.body).toEqual({
@@ -19,10 +23,13 @@ it('get a course by id', async () => {
 		},
 	});
 });
-it('return 404 for a non existing courses', async () => {
+it('return 404 for a non existing course', async () => {
+	const { token } = await MakeAuthenticatedUser('student');
 	const uuid = randomUUID();
 
-	const response = await request(server.server).get(`/courses/${uuid}`);
+	const response = await request(server.server)
+		.get(`/courses/${uuid}`)
+		.set('Authorization', token);
 
 	expect(response.status).toBe(404);
 });
